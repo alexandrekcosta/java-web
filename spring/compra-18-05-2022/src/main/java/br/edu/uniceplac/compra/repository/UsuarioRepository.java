dc1924c0cfc6258db6bd26604b62efdca5380f60
Alexandre Costa <68577176+alexandrekcosta@users.noreply.github.com>
2022-05-25 22:17:39 -0300

    Add files via upload
    Reuploading directory but with some updates

---
+++ spring/compra-18-05-2022/src/main/java/br/edu/uniceplac/compra/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.edu.uniceplac.compra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sun.xml.bind.v2.model.core.ID;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioRepository, Long> {
+	
+	
+	
+}
